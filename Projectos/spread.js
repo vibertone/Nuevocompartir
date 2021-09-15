90f255cee0b4e9efebc5c82d574418a2db911332
Victoria Bertone <Vbertone@fxinformatica.com>
2021-09-15 00:09:34 -0300

    first commit

---
+++ Projectos/spread.js
@@ -0,0 +1,22 @@
+
+//SPREAD con array
+let parte = ['los', 'cumplas'];
+let oracion = ['Que', ...parte, 'feliz'];
+
+console.log(oracion);
+
+//SPREAD con objetos
+let auto={marca:'Ferrari', km:0, anio:2019};
+let corredorUno ={nombre:'Vettel', edad: 32, ...auto};
+
+
+console.log(corredorUno);
+
+//SPREAD en una funcion
+
+function ConVariosParametros(...params){
+    //codigo a ejecutar
+}
+
+ConVariosParametros("a");
+ConVariosParametros("a","b");
