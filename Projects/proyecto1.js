70b55828c02d17c94c4dd9a95aab79059e25dded
Victoria Bertone <Vbertone@fxinformatica.com>
2021-09-16 11:43:10 -0300

    Edite el codigo de Federico en proyecto1 2 y 3

--- Projects/proyecto1.js
+++ Projects/proyecto1.js
@@ -2,4 +2,6 @@ function sumar(a, b){
     return a + b;
 }
 
-console.log(sumar(2, 4))
+console.log(sumar(2, 4));
+
+console.log(sumar(3,5)); //agregado por VB
--- Projects/proyecto3.js
+++ Projects/proyecto3.js
@@ -1,1 +1,2 @@
-let miArray = ['Amarillo', 'Rojo', 'Azul', 'Violeta', 'Negro']
+let miArray = ['Amarillo', 'Rojo', 'Azul', 'Violeta', 'Negro'];
+let miArray = ['Negro', 'Gris', 'Verde']; //agregado por VB
